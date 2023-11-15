520af905c7d524af7cf330aa1d6b62fc2b2230fd
tade18 <tade.18@seznam.cz>
2023-11-15 10:20:07 +0100

    update

--- res/js/main.js
+++ res/js/main.js
@@ -1,11 +1,16 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+const logo = new Image();
+logo.src="res/img/logo.png"
+
 
 
 let x = 10;
 let y = 10;
-let xVelocity = 0.5;
-let yVelocity = 0.5;
+let xVelocity = 3;
+let yVelocity = 3;
+let width = 190;
+let height = 190;
  
 window.addEventListener("resize", () => {
     changeCanvasSize ();
@@ -23,20 +28,20 @@ function changeCanvasSize () {
 setInterval(() => {
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  if (y + 60 >= canvas.height || y <= 0) {
+  if (y + 190 >= canvas.height || y <= 0) {
     yVelocity *= -1;
   }
-  if (x + 60 >= canvas.width || x <= 0){
+  if (x + 190 >= canvas.width || x <= 0){
     xVelocity *= -1;
   }
   x+= xVelocity;
   y+= yVelocity;
-  ctx.fillStyle = "red";
-  ctx.fillRect(x, y, 60, 60);
+  ctx.filter = `hue-rotate(${getRandomNumber(0,360)}deg)`
+  ctx.drawImage(logo, x, y, width, height)
+  ctx.filter = "none";
 }, 0.1);
 
-
-
+const getRandomNumber = (minimum, maximum) => Math.floor(Math.random()* (maximum - minimum + 1)) +minimum;
 
 
 
